d7d9cd94c14bb65e43926024bd443fff03f6d1ef
Faibysh <faybysh.b@gmail.com>
2023-12-05 20:56:26 +0100

    hw-06

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -3,11 +3,22 @@ const ctrl = require("../../controllers/auth");
 
 const { validateBody, authenticate, upload } = require("../../middlewares");
 const { schemas } = require("../../models/user");
+// const auth = require("../../controllers/auth");
 
 const router = express.Router();
 
+// signup
 router.post("/register", validateBody(schemas.registerSchema), ctrl.register);
 
+router.get("/verify/:verificationToken", ctrl.verifyEmail);
+
+router.post(
+  "/verify",
+  validateBody(schemas.emailSchema),
+  ctrl.resendVerifyEmail
+);
+
+// signin
 router.post("/login", validateBody(schemas.loginSchema), ctrl.login);
 
 router.get("/current", authenticate, ctrl.getCurrent);
